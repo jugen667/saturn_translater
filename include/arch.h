5df956e6ac318500b54801dc1b409da236e70ee5
juju80gen <julien.gty@gmail.com>
2024-11-11 19:09:25 +0100

    modified achitecture infos

--- include/arch.h
+++ include/arch.h
@@ -4,11 +4,6 @@
 
 void set_max_registers(int32_t n);
 int32_t get_num_registers();
-int32_t get_num_arch_registers();
-int32_t get_first_reg();
-int32_t get_r0();
-int32_t get_stack_reg();
-int32_t get_data_sec_start_addr();
-
+int32_t get_save_registers();
 
 #endif
--- src/arch.c
+++ src/arch.c
@@ -2,35 +2,23 @@
 
 #include "../include/arch.h"
 
-#define num_arch_registers 8
-static int32_t max_regs = num_arch_registers;
+#define working_registers   4       // A, B, C, D               = 64 bits each
+#define pointer_registers   2       // D0, D1                   = 20 bits each (five nibbles)
+#define save_registers      5       // R0, R1, R2, R3, R4, R5   = temporary area 64 bits each 
+
+// Useless to use less than 2 pointers regs and less than 4 working reg
+
+static int32_t max_save_regs = save_registers;
 
 
 void set_max_registers(int32_t n) {
-    max_regs = n;
+    max_save_regs = n;
 }
 
 int32_t get_num_registers() {
-    return max_regs;
+    return max_save_regs;
 }
 
-int32_t get_num_arch_registers() {
-    return num_arch_registers;
+int32_t get_save_registers() {
+    return save_registers;
 }
-
-int32_t get_first_reg() {
-    return 8;
-}
-
-int32_t get_r0() {
-    return 0;
-}
-
-int32_t get_stack_reg() {
-    return 29;
-}
-
-int32_t get_data_sec_start_addr() {
-    return 0x10010000;
-}
-
