95bc86d7e5742d9d013f5a09a20fdba80667b7a5
juju80gen <julien.genty80@gmail.com>
2024-12-22 00:25:19 +0100

    changed test routines

--- temp/test.c
+++ temp/test.c
@@ -2,10 +2,9 @@
 // Test Comment
 int start = 32767;
 int end = 5;
+bool x = true;
 float a = 191.01450;
-float b = -191.01450; 
-float c = -33;
-void main () {
+void main(){
 	int i, s = start , e = end ;
 	int sum = 0;
 	for (i = s; i < e; i = i + 1) {
