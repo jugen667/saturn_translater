e859d09a97a681c82ed0395388fc4854cd1db700
juju80gen <julien.genty80@gmail.com>
2024-12-21 22:44:01 +0100

    CHANGES -test routines

--- temp/test.c
+++ temp/test.c
@@ -1,12 +1,15 @@
-// Test file
-int start = 2147483648;
+/* Test file */ 
+// Test Comment
+int start = 32767;
 int end = 5;
 float a = 191.01450;
+float b = -191.01450; 
+float c = -33;
 void main () {
 	int i, s = start , e = end ;
 	int sum = 0;
 	for (i = s; i < e; i = i + 1) {
  		sum = sum + 1;	
  	}
-	// print("sum :", sum , "\n");
+ 	print("sum :", sum , "\n");
 }
