fe3a141fa669b052f70c7ff0a16eb8e9286a8ea7
juju80gen <julien.genty80@gmail.com>
2024-12-21 22:47:56 +0100

    CHANGES -added prototypes

--- include/passe_1.h
+++ include/passe_1.h
@@ -9,8 +9,15 @@
 
 #include "../include/defs.h"
 
+// DEBUG FUNCTIONS
+void print_decl_table(void);
+void print_node_info(node_t root);
 
-void analyse_passe_1(node_t root);
+// CHECKUP FUNCTIONS
+node_t get_decl_node(node_t node);
+void change_int2float(node_t node, double value);
+bool check_var_number(void);
+void check_ident_size(node_t node);
 void check_add_type(node_t node);
 void check_minus_type(node_t node);
 void check_mul_type(node_t node);
@@ -20,8 +27,5 @@ void check_main_void(node_t node);
 void check_affect_type(node_t node);
 void check_global_decl(node_t node);
 void check_intval_domain(node_t node);
-void check_floatval_domain(node_t node);
 
-
-#endif
-
+void analyse_passe_1(node_t root);
