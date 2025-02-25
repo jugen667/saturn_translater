e55ca7b37fbda51f3ef7297d5691e3dcfc155dfa
juju80gen <julien.genty80@gmail.com>
2025-02-25 22:07:57 +0100

    CHANGES - fix multiline comment

--- temp/test.c
+++ temp/test.c
@@ -1,13 +1,16 @@
-/* Test file */ 
-// Test Comment
+/* This file is a test
+ * Comment on multiple 
+ * line !
+ */ 
 int start = 0;
 int end = 5;
+// Starting main
 void main(){
 	int i;
 	start = -1-2;
 	int sum = 0;
-	// for (i = start; i < end; i = i + 1) {
- 	// 	sum = sum + 10;	
- 	// }
- 	// printf("sum :", sum , "\n");
+/*	for (i = start; i < end; i = i + 1) {
+ 		sum = sum + 10;	
+ 	}
+ 	printf("sum :", sum , "\n"); */
 }
